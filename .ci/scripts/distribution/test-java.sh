e5e3f4a968802dfa5e2160355101835fff245435
pihme <pihme@users.noreply.github.com>
2020-05-22 17:26:42 +0200

    chore(ci): run all tests, then analyze the log

--- .ci/scripts/distribution/test-java.sh
+++ .ci/scripts/distribution/test-java.sh
@@ -3,4 +3,16 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-mvn -o -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -Dzeebe.it.skip -DtestMavenId=1
+mvn -o -B -fn -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -Dzeebe.it.skip -DtestMavenId=1 | tee test.txt
+
+status=${PIPESTATUS[0]}
+
+if [[ $status != 0 ]]; then
+  rm test.txt
+  exit $status;
+fi
+
+if grep -q "There are test failures\." test.txt; then
+  rm test.txt
+  exit 1
+fi
--- .ci/scripts/distribution/test-java8.sh
+++ .ci/scripts/distribution/test-java8.sh
@@ -5,4 +5,16 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
 mvn -v
 
-mvn -o -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -pl clients/java -DtestMavenId=3
+mvn -o -B -fn -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -pl clients/java -DtestMavenId=3 | tee test.txt
+
+status=${PIPESTATUS[0]}
+
+if [[ $status != 0 ]]; then
+  rm test.txt
+  exit $status;
+fi
+
+if grep -q "There are test failures\." test.txt; then
+  rm test.txt
+  exit 1
+fi
